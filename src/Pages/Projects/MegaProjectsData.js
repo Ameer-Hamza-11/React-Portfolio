5f3167ea103733aa96b4dbec6ed4637655804ab2
ameer1hamza-bmj <ameerhamza.bmj@gmail.com>
2025-05-23 15:33:31 +0500

    js

--- src/Pages/Projects/MegaProjectsData.js
+++ src/Pages/Projects/MegaProjectsData.js
@@ -4,51 +4,60 @@ import axiosCrudImg from '../.././assets/images/axios-crud.png';
 import routerPracticeImg from '../.././assets/images/router-practice.png';
 import pokemonAppImg from '../.././assets/images/pokemon-app.png';
 import countryInfoImg from '../.././assets/images/country-info.png';
+import ecommerce from '../.././assets/images/ecommerce.png';
 
 
 const MegaProjectsData = [
   {
     id: 1,
+    title: "Ecommerce Website",
+    description: "simple Ecommerce using react, libraries & cookie to save data",
+    image: ecommerce,
+    link: "https://hamzcart.netlify.app/"
+  },
+  {
+    id: 2,
     title: "Multi-Page React App",
     description: "Multi-page website using React, Axios & Tailwind.",
     image: multiPageImg,
     link: "https://multi-page-website-react.netlify.app/"
   },
   {
-    id: 2,
+    id: 3,
     title: "React Query Site",
     description: "Uses React Query and Axios with Tailwind styling.",
     image: reactQueryImg,
     link: "https://react-query-website.netlify.app/"
   },
   {
-    id: 3,
+    id: 4,
     title: "Axios CRUD",
     description: "CRUD operations with Axios and mock API.",
     image: axiosCrudImg,
     link: "https://axios-crud-operations-website.netlify.app/"
   },
   {
-    id: 4,
+    id: 5,
     title: "Router Practice",
     description: "React Router demo with page transitions.",
     image: routerPracticeImg,
     link: "https://react-routers-practice.netlify.app/"
   },
   {
-    id: 5,
+    id: 6,
     title: "Pokémon Cards",
     description: "Pokémon API project with stats and themes.",
     image: pokemonAppImg,
     link: "https://react-pokemon-website.netlify.app/"
   },
   {
-    id: 6,
+    id: 7,
     title: "Country Info",
     description: "Shows country info using external API.",
     image: countryInfoImg,
     link: "https://react-country-website-hamza.netlify.app/"
-  }
+  },
+
 ];
 
 export default MegaProjectsData;
